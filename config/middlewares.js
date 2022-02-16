12ecbabd001486bda218d1a76b9ede393e1f37ea
Christher Lenander <christher.lenander@enklare.se>
2022-02-16 02:44:57 +0100

    update middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,9 +6,9 @@ module.exports = ({ env }) => [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", env("MIDDLEWARE_CONNECT_SRC")],
-          "img-src": ["'self'", "data:", "blob:", env("MY_HEROKU_URL")],
-          "media-src": ["'self'", "data:", "blob:", env("MY_HEROKU_URL")],
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", `${env("MY_HEROKU_URL")}`],
+          "media-src": ["'self'", "data:", "blob:", `${env("MY_HEROKU_URL")}`],
           upgradeInsecureRequests: null,
         },
       },
