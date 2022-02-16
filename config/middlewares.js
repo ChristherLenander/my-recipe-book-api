e0386760535f49b5a1a0023af745a8c5888a5025
Christher Lenander <christher.lenander@enklare.se>
2022-02-16 02:31:36 +0100

    updated middleware.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,24 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", env("MIDDLEWARE_CONNECT_SRC")],
+          "img-src": ["'self'", "data:", "blob:", env("MY_HEROKU_URL")],
+          "media-src": ["'self'", "data:", "blob:", env("MY_HEROKU_URL")],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public",
 ];
