c71ff1857ce2f6017b16efc2a70fba426cb50467
Christher Lenander <christher.lenander@enklare.se>
2022-02-17 01:54:56 +0100

    added mailgun as email provider

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,19 @@
 module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "mailgun",
+      providerOptions: {
+        apiKey: env("MAILGUN_API_KEY"),
+        domain: env("MAILGUN_DOMAIN"), //Required if you have an account with multiple domains
+        host: env("MAILGUN_HOST"), // we have only 2 options here. api.mailgun.net for US servers, api.eu.mailgun.net for EU Servers.
+      },
+      settings: {
+        defaultFrom: env("EMAIL_DEFAULT_FROM"),
+        defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"),
+        testAddress: env("EMAIL_TEST_ADDRESS"),
+      },
+    },
+  },
   upload: {
     config: {
       provider: "strapi-provider-upload-dos",
